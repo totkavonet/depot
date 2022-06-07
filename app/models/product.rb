158dd0d36a8b7989b50b1060a053b9684c3ab475
PavelLoiko <loikapavel2405@gmail.com>
2022-06-07 16:16:56 +0300

    C1 complete

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
+  validates :title, uniqueness: true, length: { in: 10..20, message: 'can not be blank and should be greater then 10 and less then 20' }
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
